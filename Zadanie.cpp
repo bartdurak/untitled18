b194a0a034185cdb7eac79abdc927ecd130ddfbf
Bart Durak <projektator@gmail.com>
2023-04-02 12:47:35 +0200

    Może być. Ważne żeby działało.

--- Zadanie.cpp
+++ Zadanie.cpp
@@ -1,13 +1,16 @@
 #include "Zadanie.h"
 #include <fstream>
 #include <iostream>
+
+// stosuj Ctrl + Alt + l / to justuje tekst
 using namespace std;
 int tablica[5];
 int tablica1[5];
 int poprawne = 1;
 int n;
+
 void funkcje::dodawanieTablic() {
-    cout << "ile liczb chcesz dac do tablicy?";
+    cout << "ile liczb chcesz dać do tablicy?";
     cin >> n;
     for (int i = 0; i < n; i++) {
         cout << "podaj " << i + 1 << " liczbe: ";
@@ -19,33 +22,33 @@ void funkcje::dodawanieTablic() {
     }
 
 }
+
 void funkcje::zapisPliku() {
     fstream plik;
-    plik.open("takok.txt", ios::out);
+    plik.open("takok.txt", ios::out); // gdzie masz plik takok.txt ?
+    //znalazłem plik. Nazywa się untitled18. W kodzie piszesz otwórz plik takok.txt
     if (plik.is_open()) {
-        plik << "otworzyles go: "<<endl;
+        plik << "otworzyles go: " << endl;
         cout << "poprawnie otwarto plik \n";
-        plik<<"tablica pierwsza: ";
-        for (int i = 0; i < n; i++)
-        {
+        plik << "tablica pierwsza: ";
+        for (int i = 0; i < n; i++) {
             plik << tablica[i] << " ";
         }
-        plik<<endl<<"druga tablica: ";
-        for(int x=0;x<n;x++){
+        plik << endl << "druga tablica: ";
+        for (int x = 0; x < n; x++) { //sam wymyśliłeś x?/ zwyczaj jest taki i,j,k,l
+            //to jest związane z oznaczaniem kolumny i wiersza w tablicach macierzy. tak się przyjęło.
+
             plik << tablica1[x] << " ";
         }
 
 
-
-
-
-
         plik.close();
     } else {
-        cout << "nie moge otworzyc badz stworzyc pliku";
+        cout << "nie moge otworzyc badź stworzyć pliku";  //
     }
 
 }
+
 void funkcje::poruwnywanieTablic() {
     for (int i = 0; i < n; ++i) {
         if (tablica[i] != tablica1[i]) poprawne = 0;
@@ -53,8 +56,7 @@ void funkcje::poruwnywanieTablic() {
     }
     if (poprawne) {
         cout << "te dwie tablice maja identyczne liczby ";
-    }
-    else {
+    } else {
         cout << "te dwie tablice nie maja identycznych liczb";
     }
 
--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <fstream>
 #include "Zadanie.h"
+//czy zadanie z funcji i tablic moze byc w jednym pliku obiektowym.
+// może być. Nazywaj pliki zgodnie z ich przeznaczeniem. Nie Zadanie tylko tablica, funkcja.
+// gdzie znalazłeś instrukcję żeby pisać nazwy plików obiektu od dużej litery?
 
 
 int main()
